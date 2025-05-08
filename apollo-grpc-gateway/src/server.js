cb96f367052052a65567bbc8fd1f7ea869678317
antonio.sanchez <antonio.sanchez@gofore.com>
2025-05-08 08:10:51 +0300

    clean up subscriptions

--- apollo-grpc-gateway/src/server.js
+++ apollo-grpc-gateway/src/server.js
@@ -1,56 +1,65 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import { createServer } from 'http';
+import { makeExecutableSchema } from '@graphql-tools/schema';
+import { WebSocketServer } from 'ws';
 
+// Seen in https://the-guild.dev/graphql/ws/get-started
+import { useServer } from 'graphql-ws/use/ws';
 import { PubSub } from 'graphql-subscriptions';
-
-/*
-const { PubSub } = require('graphql-subscriptions');
-
-const typeDefs = require('./schema/typeDefs');
-const resolvers = require('./resolvers');
-
-*/
-
+import bodyParser from 'body-parser';
 import typeDefs from './schema/typeDefs.js';
 import resolvers from './resolvers/index.js';
 
+const PORT = 4000;
 
+// seen in https://github.com/apollographql/docs-examples/blob/main/apollo-server/v4/subscriptions-graphql-ws/src/index.ts
 const pubsub = new PubSub();
 
+// Create schema, which will be used separately by ApolloServer and
+// the WebSocket server.
+const schema = makeExecutableSchema({ typeDefs, resolvers });
+
+// Create an Express app and HTTP server; we will attach the WebSocket
+// server and the ApolloServer to this HTTP server.
+const app = express();
+const httpServer = createServer(app);
+
+// Set up WebSocket server.
+const wsServer = new WebSocketServer({
+  server: httpServer,
+  path: '/graphql',
+});
+
+const serverCleanup = useServer({ schema }, wsServer);
+
+// Set up ApolloServer.
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: () => ({ pubsub }),
-  subscriptions: {
-    path: '/subscriptions',
-    onConnect: () => {
-      console.log('Client connected for subscriptions');
+  schema,
+  plugins: [
+    // Proper shutdown for the HTTP server.
+    ApolloServerPluginDrainHttpServer({ httpServer }),
+
+    // Proper shutdown for the WebSocket server.
+    {
+      async serverWillStart() {
+        return {
+          async drainServer() {
+            await serverCleanup.dispose();
+          },
+        };
+      },
     },
-    onDisconnect: () => {
-      console.log('Client disconnected from subscriptions');
-    },
-  },
+  ],
 });
 
-/*
-server.listen().then(({ url, subscriptionsUrl }) => {
-  console.log(`🚀 Server ready at ${url}`);
-  console.log(`🚀 Subscriptions ready at ${subscriptionsUrl}`);
-});*/
+await server.start();
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-
-//  1. creates an Express app
-
-//  2. installs your ApolloServer instance as middleware
-
-//  3. prepares your app to handle incoming requests
-
-const { url } = await startStandaloneServer(server, {
-
-  listen: { port: 4000 },
-
+// Now that our HTTP server is fully set up, actually listen.
+httpServer.listen(PORT, () => {
+  console.log(`🚀 Query endpoint ready at http://localhost:${PORT}/graphql`);
+  console.log(`🚀 Subscription endpoint ready at ws://localhost:${PORT}/graphql`);
 });
 
-
-console.log(`🚀  Server ready at: ${url}`);
