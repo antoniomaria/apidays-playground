a54d642c9a3f7ae82df60eae0057de4081c5ba70
antonio.sanchez <antonio.sanchez@gofore.com>
2025-05-18 19:41:49 +0300

    split aircraft seats in two: left and right

---
+++ aircraft-seats-map/aircraft-seats-map/src/components/LeftWingSeats.js
@@ -0,0 +1,267 @@
+import React from 'react';
+
+const LeftWingSeats = () => {
+  return (
+    <div className="seats">
+    <div className="seats-triple first-line" data-line="1">
+      <div data-letter="C" className="active seat">
+      </div>
+      <div data-letter="B" className="active seat">
+      </div>
+      <div data-letter="A" className="active seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="2">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="3">
+      <div className="empty seat">
+      </div>
+      <div className="empty seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="4">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="5">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="6">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="7">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="8">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="9">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="10">
+      <div className="empty seat">
+      </div>
+      <div className="empty seat">
+      </div>
+      <div className="empty seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="11">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="12">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="13">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="14">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="15">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="16">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="17">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="18">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="19">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="20">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="21">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="22">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="23">
+      <div className="empty seat">
+      </div>
+      <div className="empty seat">
+      </div>
+      <div className="empty seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="24">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="25">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="26">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="27">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="28">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="29">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="30">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple" data-line="31">
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+      <div className="seat">
+      </div>
+    </div>
+    <div className="seats-triple last-line" data-line="32">
+      <div data-letter="C" className="seat empty">
+      </div>
+      <div data-letter="B" className="seat empty">
+      </div>
+      <div data-letter="A" className="seat empty">
+      </div>
+    </div>
+
+  </div>
+  );
+};
+
+export default LeftWingSeats;
---
+++ aircraft-seats-map/aircraft-seats-map/src/components/RightWingSeats.js
@@ -0,0 +1,266 @@
+import React from 'react';
+
+const RightWingSeats = () => {
+  return (
+    <div className="seats">
+      <div className="seats-triple first-line">
+        <div data-letter="F" className="active seat">
+        </div>
+        <div data-letter="E" className="active seat">
+        </div>
+        <div data-letter="D" className="active seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="active seat">
+        </div>
+        <div className="active seat">
+        </div>
+        <div className="active seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="active seat">
+        </div>
+        <div className="active seat">
+        </div>
+        <div className="active seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="active seat">
+        </div>
+        <div className="active seat">
+        </div>
+        <div className="active seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="empty seat">
+        </div>
+        <div className="empty seat">
+        </div>
+        <div className="empty seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="empty seat">
+        </div>
+        <div className="empty seat">
+        </div>
+        <div className="empty seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple">
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+        <div className="seat">
+        </div>
+      </div>
+      <div className="seats-triple last-line">
+        <div data-letter="F" className="seat empty">
+        </div>
+        <div data-letter="E" className="seat empty">
+        </div>
+        <div data-letter="D" className="seat empty">
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightWingSeats;
