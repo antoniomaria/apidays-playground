26f17fe3c57f5e637e6b62b59c96c033742dcca8
Antonio Sanchez <antonio.sanchez@landisgyr.com>
2025-05-19 14:53:24 +0300

    example graphql query

--- aircraft-seats-websocket-ui/src/App.js
+++ aircraft-seats-websocket-ui/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import Aircraft from './components/Aircraft';
+import Ping from './components/Ping';
 
 function App() {
   return (
+    <div>
+      <div>
+        <h1>Ping Test</h1>
+        <Ping />
+      </div>      
       <Aircraft />
+    </div>
   );
 }
 
